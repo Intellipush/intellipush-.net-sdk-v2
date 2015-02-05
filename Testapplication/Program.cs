68b516330ef226cb71117cb5b9f97cdbd1193f4c
Olav Hermansen <olavzat@gmail.com>
2015-02-05 14:19:21 +0100

    Init: Basic structure

---
+++ Testapplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using smslib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SMSClient c = new SMSClient("", "");
+            Console.WriteLine(c.postToUrl());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ smslib/SMSClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smslib
+{
+    public class SMSClient
+    {
+
+        private string customerKey = "";
+        private string applicationKey = "";
+
+        // Settings
+
+        private const string API_URL = "http://192.168.1.138";
+
+        public SMSClient(string customerKey, string applicationKey)
+        {
+            this.customerKey = customerKey;
+            this.applicationKey = applicationKey;
+        }
+
+        public string postToUrl()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(API_URL);
+
+            var list = new List<KeyValuePair<string, string>>();
+
+
+            list.Add(new KeyValuePair<string,string>("asd", "asd"));
+
+            var postData = "thing1=hello";
+            postData += "&thing2=world";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            return responseString;
+        }
+
+
+    }
+}
